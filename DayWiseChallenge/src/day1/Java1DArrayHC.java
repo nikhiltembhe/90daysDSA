52e259169184bc32860a37e4bf2fce05bf014320
nikhiltembhe <nikhiltembhe@gmail.com>
2022-05-13 00:19:06 +0530

    Getting started : Day 1 commit

---
+++ DayWiseChallenge/src/day1/Java1DArrayHC.java
@@ -0,0 +1,26 @@
+package day1;
+
+import java.util.Scanner;
+
+// question :- https://www.hackerrank.com/challenges/java-1d-array-introduction/problem?isFullScreen=false
+public class Java1DArrayHC {
+
+        public static void main(String[] args) {
+
+            Scanner scan = new Scanner(System.in);
+            int n = scan.nextInt();
+
+            int[] a = new int[n];
+            for(int i =0; i<n; i++){
+                a[i] = scan.nextInt();
+            }
+            scan.close();
+
+            // Prints each sequential element in array a
+            for (int i = 0; i < a.length; i++) {
+                System.out.println(a[i]);
+            }
+        }
+    }
+
+
